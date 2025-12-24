e4b7441557142717a0a027eb91177feb24b10616
Yjaatouri <yahyajaatouri2000@gmail.com>
2025-12-24 15:26:52 +0100

    complete php code structure

--- BlogCMC.php
+++ BlogCMC.php
@@ -7,8 +7,20 @@
         private string $status;
         private string $author;
         private string $createdAtA;
-        private DateTime $publishedAt;
+        private DateTime $publishedAtA;
         private DateTime $updatedAtA;
+        function __construct($id_article,$titre,$content,$excerpt, $status,$author,$createdAtA,$publishedAtA,$updatedAtA)
+        {
+            $this->id_article = $id_article;
+            $this->titre = $titre;
+            $this->content = $content;
+            $this->excerpt = $excerpt;
+            $this->status = $status;
+            $this->author = $author;
+            $this->createdAtA = $createdAtA;
+            $this->publishedAtA = $publishedAtA;
+            $this->updatedAtA = $updatedAtA;
+        }
         public function addCategory (){
 
         }
@@ -20,17 +32,33 @@
         }
     }
         class comment {
-        private string $id_comment ;
+        private int $id_comment ;
         private string $libelle;
         private string $description;
         private DateTime $dateC;
+        function __construct($id_comment,$libelle,$description,$dateC)
+        {
+            $this->id_comment = $id_comment;
+            $this->libelle = $libelle;
+            $this->description = $description;
+            $this->dateC = $dateC;
+        }
+        
 
     }
         class categorie {
         private int $id_categorie;
-        private string $name;
+        private string $nameCa;
         private string $description;
         private DateTime $dateCA;
+        function __construct($id_categorie, $nameCa ,$description ,$dateCA)
+        {
+         $this->id_categorie = $id_categorie;
+         $this->nameCa = $nameCa;
+         $this->description = $description;
+         $this->dateCA = $dateCA;
+
+        }
         public function getParent(){
 
         }
@@ -45,6 +73,15 @@ class user {
     protected string $password;
     protected DateTime $createdAT; 
     protected DateTime $lastLogin;
+    function __construct($id_user , $username , $email , $password , $createdAT , $lastLogin){
+        $this->id_user = $id_user;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password  = $password;
+        $this->createdAT = $createdAT;
+        $this->lastLogin = $lastLogin;
+    }
+    
 
     public function login($email , $password){
 
@@ -55,25 +92,31 @@ class user {
 
 }
 class moderateur extends user {
-    public function approvComment(){
+    public function approvComment($id_comment){
         
     }
-    public function deleteCommnet(){
+    public function deleteCommnet($id_comment){
 
     }
-    public function createCategory(){
+    public function createCategory($nameCa){
 
     } 
-    public function publishArticle(){
+    public function deleteCategorie($id_categorie){
 
     }
-    public function deleteAnyArticle(){
+    public function publishArticle($id_article){
+
+    }
+    public function deleteAnyArticle($id_article){
 
     }
     
 }
 class author extends user {
      private string $bio;
+     function __construct($bio){
+        $this->bio = $bio;
+     }
      public function createArticle(){
 
      }
@@ -89,13 +132,17 @@ class author extends user {
     }
     class admin extends moderateur {
         private bool $isSuperAdmin;
-        public function createUser(){
+        function __construct($isSuperAdmin){
+            $this->isSuperAdmin = $isSuperAdmin;
 
         }
-        public function deleteUser(){
+        public function createUser($username , $email ,$password){
 
         }
-        public function updateUserRole(){
+        public function deleteUser($id_user){
+
+        }
+        public function updateUserRole($id_user){
 
         }
         public function listAllUsers (){
